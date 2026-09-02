14de8b17dfc76a7487f88c8e8e5c52a881631f58
BinaryRishabh <srivastavarishabh2001@gmail.com>
2026-09-02 20:10:04 +0530

    feat(canvas): add move action to undo/redo contract for drag-to-move

--- shared/types/UndoCanvasResourceAction.types.ts
+++ shared/types/UndoCanvasResourceAction.types.ts
@@ -2,6 +2,7 @@ import type { Resource } from "../interface/Resource.interface";
 import type { ConnectionLine } from "../interface/ConnectionLine.interface";
 
 // Set the state for undo/redo of resource elements on the canvas.
-export type UndoCanvasResourceAction = 
-  | { type: "delete"; resource: Resource; connectionLines: ConnectionLine[]; savedState: boolean }
-  | { type: "add"; resource: Resource; connectionLines: ConnectionLine[]; savedState: boolean }
+export type UndoCanvasResourceAction =
+| { type: "delete"; resource: Resource; connectionLines: ConnectionLine[]; savedState: boolean }
+| { type: "add"; resource: Resource; connectionLines: ConnectionLine[]; savedState: boolean }
+| { type: "move"; resourceId: string; fromX: number; fromY: number; toX: number; toY: number; savedState: boolean };
